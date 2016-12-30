8d6aeefc6c7ea320e9ecd349a8da662ab3cdd814
Ivan <ivan.joji81@gmail.com>
2016-12-30 23:48:22 +0100

    kreiranje posta i prikaz gotovi, stim da Category je za sada staticki

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -8,6 +8,7 @@ use App\Role;
 use App\Photo;
 use App\Http\Requests\UsersRequest;
 use App\Http\Requests\UsersEditRequest;
+use Illuminate\Support\Facades\Session;
 
 class AdminUsersController extends Controller
 {
@@ -115,6 +116,10 @@ class AdminUsersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        unlink(public_path() . $user->photo->file);
+        $user->delete();
+        Session::flash('deleted_user', 'User je obrisan');
+        return redirect('/admin/users');
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+	<h1 class="text-center">Oppsss no page avaiable!</h1>
+@endsection
+
